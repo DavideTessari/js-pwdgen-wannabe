29d2d34c6ec295891d5afb44aeef4c8baebaf823
DavideTessari <davtex@gmail.com>
2024-03-05 09:51:26 +0100

    feat: bonus complete

--- js/script.js
+++ js/script.js
@@ -9,10 +9,25 @@ const userSurname = prompt('Dimmi il cognome');
 // Chiedi il colore preferito
 const userColor = prompt('Dimmi il tuo colore preferito');
 
+// Chiediamo un numero a sua scelta
+
+// Ci servono: anno corrente e l'età dell'utente
+const currentNumber = 23;
+
+// chiedo l'età all'utente
+const userNumber = prompt('Dimmi un numero a tuo piacere');
+
+// Converto userAge in un tipo di dato numero
+const userNumberAsNumeric = parseInt(userNumber);
+
+// Somma numeri
+const sum = currentNumber + userNumberAsNumeric;
+
+
 
 // Stampo il risultato nomecognomecolorepreferito23
 
-const userMessage = `${userName}${userSurname}${userColor}23`;
+const userMessage = `${userName}${userSurname}${userColor}${sum}`;
 
 // alert(userMessage);
 document.getElementById('message').innerHTML = userMessage;
